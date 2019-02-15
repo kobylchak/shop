f3b4f72f58007d7b148905b15e41b26c7218f61d
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-02-15 13:53:16 +0200

    Add PhotoExceptions

---
+++ src/main/java/ua/shop/exception/PhotoErrorException.java
@@ -0,0 +1,8 @@
+package ua.shop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class PhotoErrorException extends RuntimeException {
+}
