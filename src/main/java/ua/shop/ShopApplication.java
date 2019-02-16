8747904a17c9703a209be483bcf9e76ebb94c375
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-02-16 22:27:49 +0200

    delete not needed user

--- src/main/java/ua/shop/ShopApplication.java
+++ src/main/java/ua/shop/ShopApplication.java
@@ -6,26 +6,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import ua.shop.dao.CustomUser;
 import ua.shop.dao.UserRole;
-import ua.shop.service.ProductService;
+import ua.shop.service.MobileService;
 import ua.shop.service.UserService;
 
 @SpringBootApplication
 public class ShopApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(ShopApplication.class, args);
     }
 
-
     @Bean
-    public CommandLineRunner demo(final ProductService productService, final UserService userService) {
+    public CommandLineRunner demo(final MobileService mobileService, final UserService userService) {
         return new CommandLineRunner() {
             @Override
             public void run(String... strings) throws Exception {
                 userService.addUser(new CustomUser("admin", "$2a$10$QTLi3Raw8MPC1XTk83d63.n8STr3p.hgqGwhD1yWjmzTU0XmRuj36", UserRole.ADMIN));
                 userService.addUser(new CustomUser("user", "$2a$10$s0nvPz6NO8oOvIvFYljjJekQ3BkXgDy2NZsjOawQH8Gj1cNfLp9SO", UserRole.USER));
-                userService.addUser(new CustomUser("aa", "a", UserRole.USER));
             }
         };
     }
