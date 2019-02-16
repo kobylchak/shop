c617c3cad85720f6d2174e35c4aaa71f69f90293
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-02-16 22:50:26 +0200

    Add UserRepository

---
+++ src/main/java/ua/shop/dao/impl/UserRepository.java
@@ -0,0 +1,14 @@
+package ua.shop.dao.impl;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ua.shop.dao.CustomUser;
+
+public interface UserRepository extends JpaRepository<CustomUser, Long> {
+    @Query("SELECT u FROM CustomUser u where u.login = :login")
+    CustomUser findByLogin(@Param("login") String login);
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM CustomUser u WHERE u.login = :login")
+    boolean existsByLogin(@Param("login") String login);
+}
