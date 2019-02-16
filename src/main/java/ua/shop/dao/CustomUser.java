e2826f3a1ef1d5383e7523cd28ed988c202e3873
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-02-16 22:48:44 +0200

    Add CustomUser entity

---
+++ src/main/java/ua/shop/dao/CustomUser.java
@@ -0,0 +1,108 @@
+package ua.shop.dao;
+
+import javax.persistence.*;
+
+@Entity
+public class CustomUser {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String login;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+    private String name;
+    private String surname;
+    private String email;
+    private String phone;
+    private String address;
+
+    public CustomUser(String login, String password, UserRole role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    public CustomUser(String login, String password, UserRole role, String email, String phone) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public CustomUser() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
