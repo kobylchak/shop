476c34fd62f97e5700d942c182399157c91b24dd
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-02-17 16:14:55 +0200

    Add change to MyController

--- src/main/java/ua/shop/controller/MyController.java
+++ src/main/java/ua/shop/controller/MyController.java
@@ -3,31 +3,21 @@ package ua.shop.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import ua.shop.dao.*;
-import ua.shop.exception.PhotoErrorException;
-import ua.shop.exception.PhotoNotFoundException;
 import ua.shop.service.PhotoService;
 import ua.shop.service.MobileService;
 import ua.shop.service.UserService;
 
-import java.io.IOException;
 import java.util.List;
 
 @Controller
 public class MyController {
-    private static final int DEFAULT_BRAND_ID = -1;
-    private static final int DEFAULT_MOBILE_ID = -1;
     private static final int ITEMS_PER_PAGE = 6;
     @Autowired
     private UserService userService;
@@ -115,98 +105,8 @@ public class MyController {
         return "admin";
     }
 
-//    @GetMapping("/find_users")
-//    public String findUsers(Model model){
-//        List<CustomUser> users = userService.findAllCustomUser();
-//        model.addAttribute("users", users);
-//        return "users_page";
-//    }
-
-
-//    @RequestMapping("/brand_add_page")
-//    public String brandAddPage() {
-//        return "brand_add_page";
-//    }
-
-    @RequestMapping("/photo_add_page")
-    public String photoAddPage(Model model) {
-        model.addAttribute("brands", mobileService.findBrands());
-        return "photo_add_page";
-    }
-
-//    @RequestMapping("/brand/{id}")
-//    public String listBrand(@PathVariable(value = "id") long brandId,
-//                            @RequestParam(required = false, defaultValue = "0") Integer page,
-//                            Model model) {
-//        Brand brand = (brandId != DEFAULT_BRAND_ID) ? mobileService.findBrand(brandId) : null;
-//        if (page < 0) page = 0;
-//        List<Mobile> mobiles = mobileService.findByBrand(brand, new PageRequest(page, ITEMS_PER_PAGE, Sort.Direction.DESC, "id"));
-//        model.addAttribute("brands", mobileService.findBrands());
-//        model.addAttribute("mobiles", mobiles);
-//        model.addAttribute("byGroupPages", getPageCount(brand));
-//        model.addAttribute("brandId", brandId);
-//        return "admin";
-//    }
-
-
-
-    @PostMapping("/photo/delete")
-    public ResponseEntity<Void> deletePhoto(@RequestParam(value = "toDeletePhoto[]", required = false) long[] toDelete) {
-        if (toDelete != null && toDelete.length > 0)
-            photoService.deletePhotos(toDelete);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @PostMapping("/photo/add")
-    public String photoAdd(Model model,
-                           @RequestParam(value = "mobileId") long mobileId,
-                           @RequestParam MultipartFile photo) {
-        try {
-            Mobile mobile = (mobileId != DEFAULT_MOBILE_ID) ? mobileService.findMobileById(mobileId) : null;
-            mobile.getPhotos().add(new Photo(mobile, photo.getBytes()));
-            mobileService.saveMobile(mobile);
-            return "redirect:/admin";
-        } catch (IOException ex) {
-            throw new PhotoErrorException();
-        }
-    }
-
-    @GetMapping("/download/photo/{mobile.id}")
-    public String downloadPhoto(@PathVariable("mobile.id") long id,
-                                Model model) {
-        model.addAttribute("mobileId", id);
-        return "photo_add_page";
-    }
-
-//    @RequestMapping(value = "/brand/add", method = RequestMethod.POST)
-//    public String brandAdd(@RequestParam String name) {
-//        mobileService.addBrand(new Brand(name));
-//        return "redirect:/admin";
-//    }
-
-    @RequestMapping("/photo/{photo.id}")
-    public ResponseEntity<byte[]> onPhoto(@PathVariable("photo.id") long id) {
-        return photoById(id);
-    }
-
-    private ResponseEntity<byte[]> photoById(long id) {
-        Photo photo = photoService.findPhotoById(id);
-        byte[] bytes = photo.getPhoto();
-        if (bytes == null)
-            throw new PhotoNotFoundException();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_PNG);
-        return new ResponseEntity<byte[]>(bytes, headers, HttpStatus.OK);
-    }
-
-
     private long getPageCount() {
         long totalCount = mobileService.count();
         return (totalCount / ITEMS_PER_PAGE) + ((totalCount % ITEMS_PER_PAGE > 0) ? 1 : 0);
     }
-//
-//    private long getPageCount(Brand brand) {
-//        long totalCount = mobileService.countByBrand(brand);
-//        return (totalCount / ITEMS_PER_PAGE) + ((totalCount % ITEMS_PER_PAGE > 0) ? 1 : 0);
-//    }
 }
