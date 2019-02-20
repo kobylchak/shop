f3bae1161a43c35416f9fab68e28c9efb132dc63
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-02-20 03:06:25 +0200

    Add some change to BasketRepository

--- src/main/java/ua/shop/dao/impl/BasketRepository.java
+++ src/main/java/ua/shop/dao/impl/BasketRepository.java
@@ -1,19 +1,10 @@
 package ua.shop.dao.impl;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ua.shop.dao.Basket;
-import ua.shop.dao.Mobile;
-
-import java.util.List;
 
 public interface BasketRepository extends JpaRepository<Basket, Long> {
 
-//    @Query("SELECT m FROM Mobile m WHERE m.basket = :basket")
-//    List<Mobile> findByBasket(@Param("basket") Basket basket);  //+++++++++++++++
-
     Basket findBasketByName(String name);
-
 
 }
