85eaffe47da52738cfae2b3637b85d73f894a199
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-02-23 19:11:01 +0200

    Change BasketRepository

--- src/main/java/ua/shop/dao/impl/BasketRepository.java
+++ src/main/java/ua/shop/dao/impl/BasketRepository.java
@@ -1,6 +1,5 @@
 package ua.shop.dao.impl;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,9 +11,13 @@ import java.util.List;
 public interface BasketRepository extends JpaRepository<Basket, Long> {
 
     @Query("SELECT b FROM Basket b WHERE b.us = :us")
-    List<Basket> findByUs(@Param("us")CustomUser us);
+    List<Basket> findByUs(@Param("us") CustomUser us);
+
+    @Query("SELECT b FROM Basket b WHERE b.us = :us and b.paid = :paid")
+    List<Basket> findByUsAndPaidEquals(@Param("us") CustomUser us, @Param("paid") String paid);
 
     Basket findBasketByName(String name);
+
     Basket findById(long id);
 
 
