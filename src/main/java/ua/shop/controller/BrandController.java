da00d9d5dcdc9c2155915b09a2702e6d410735b7
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-02-24 22:48:57 +0200

    Change BrandController.Add posibility find all brands

--- src/main/java/ua/shop/controller/BrandController.java
+++ src/main/java/ua/shop/controller/BrandController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ua.shop.dao.Brand;
 import ua.shop.dao.Mobile;
+import ua.shop.service.BrandService;
 import ua.shop.service.MobileService;
 
 import java.util.List;
@@ -23,39 +24,50 @@ public class BrandController {
     @Autowired
     private MobileService mobileService;
 
+    @Autowired
+    private BrandService brandService;
+
     @GetMapping
     public String brandAddPage() {
         return "brand_add_page";
     }
 
+    @PostMapping
+    public String brandAdd(@RequestParam String name) {
+        if (checkExistBrandName(name)) return "redirect:/admin";
+        brandService.addBrand(new Brand(name));
+        return "redirect:/admin";
+    }
+
     @GetMapping("/{id}")
-    public String listBrand(@PathVariable(value = "id") long brandId,
+    public String findMobilesByBrand(@PathVariable(value = "id") long brandId,
                             @RequestParam(required = false, defaultValue = "0") Integer page,
                             Model model) {
-        Brand brand = mobileService.findBrand(brandId);
+        Brand brand = brandService.findBrandById(brandId);
         if (page < 0) page = 0;
         List<Mobile> mobiles = mobileService.findByBrand(brand, new PageRequest(page, ITEMS_PER_PAGE, Sort.Direction.DESC, "id"));
-        model.addAttribute("brands", mobileService.findBrands());
+        model.addAttribute("brands", brandService.findBrands());
         model.addAttribute("mobiles", mobiles);
         model.addAttribute("byGroupPages", getPageCount(brand));
         model.addAttribute("brandId", brandId);
         return "admin";
     }
 
-    @PostMapping(value = "/add")
-    public String brandAdd(@RequestParam String name) {
-        mobileService.addBrand(new Brand(name));
-        return "redirect:/admin";
+    @ResponseBody
+    @GetMapping("/all")
+    public List<Brand> showAllBrand(){
+        return brandService.findBrands();
     }
 
+    private boolean checkExistBrandName(String name) {
+        long countSameName = brandService.findBrands().stream()
+                .filter(brand -> brand.checkName(name))
+                .count();
+        return countSameName != 0;
+    }
 
     private long getPageCount(Brand brand) {
         long totalCount = mobileService.countByBrand(brand);
         return (totalCount / ITEMS_PER_PAGE) + ((totalCount % ITEMS_PER_PAGE > 0) ? 1 : 0);
     }
-
-//    private long getPageCount() {
-//        long totalCount = mobileService.count();
-//        return (totalCount / ITEMS_PER_PAGE) + ((totalCount % ITEMS_PER_PAGE > 0) ? 1 : 0);
-//    }
 }
