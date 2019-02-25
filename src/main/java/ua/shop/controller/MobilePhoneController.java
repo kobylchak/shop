c62840e8417e779f885498d2453c73e360c0e517
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-02-25 21:14:55 +0200

    Add MobilePhoneController

---
+++ src/main/java/ua/shop/controller/MobilePhoneController.java
@@ -0,0 +1,46 @@
+package ua.shop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ua.shop.dao.Brand;
+import ua.shop.dao.Mobile;
+import ua.shop.dao.MobilePhone;
+import ua.shop.service.BrandService;
+import ua.shop.service.MobilePhoneService;
+import ua.shop.service.MobileService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/mobilephone")
+public class MobilePhoneController {
+    @Autowired
+    private MobilePhoneService mobilePhoneService;
+    @Autowired
+    private BrandService brandService;
+    @Autowired
+    private MobileService mobileService;
+
+    @GetMapping("/{brand.id}")
+    public String addMobilePhone(Model model,
+                                 @PathVariable(value = "brand.id") long brandId){
+        Brand brand = brandService.findBrandById(brandId);
+        List<Mobile> mobiles = mobileService.findByBrand(brand, Pageable.unpaged());
+        model.addAttribute("mobiles", mobiles);
+        return "mobilephone_add_page";
+    }
+
+    @PostMapping
+    public String mobileAdd(Model model,
+                            @RequestParam(value = "mobile") long mobileId,
+                            @RequestParam String imei){
+        Mobile mobile = mobileService.findMobileById(mobileId);
+        MobilePhone mobilePhone = new MobilePhone(imei, mobile);
+        mobilePhoneService.saveMobilePhone(mobilePhone);
+        return "redirect:/admin";
+    }
+
+}
