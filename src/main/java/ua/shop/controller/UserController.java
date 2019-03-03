10a12c98f013a77b91d7c23f2845c1ee6da8c24e
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-03-03 20:59:31 +0200

    Change UserController

--- src/main/java/ua/shop/controller/UserController.java
+++ src/main/java/ua/shop/controller/UserController.java
@@ -1,17 +1,18 @@
 package ua.shop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ua.shop.dao.Basket;
-import ua.shop.dao.CustomUser;
-import ua.shop.dao.UserRole;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ua.shop.dao.*;
 import ua.shop.service.BasketService;
+import ua.shop.service.BrandService;
 import ua.shop.service.MobileService;
 import ua.shop.service.UserService;
 
@@ -21,17 +22,23 @@ import java.util.List;
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    private static final int ITEMS_PER_PAGE = 6;
+
     @Autowired
-    private BasketService basketService;
+    private BrandService brandService;
 
     @Autowired
     private MobileService mobileService;
 
     @Autowired
+    private BasketService basketService;
+
+    @Autowired
     private UserService userService;
 
+
     @GetMapping
-    public String findUsers(Model model){
+    public String findUsers(Model model) {
         List<CustomUser> users = userService.findAllCustomUser();
         model.addAttribute("users", users);
         return "users_page";
@@ -43,24 +50,47 @@ public class UserController {
             userService.deleteUser(toDelete);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
     @RequestMapping(value = "/newuser", method = RequestMethod.POST)
-    public String update(@RequestParam String login,
+    public String update(@RequestParam String first,
+                         @RequestParam String last,
+                         @RequestParam String login,
+                         @RequestParam String email,
                          @RequestParam String password,
-                         @RequestParam(required = false) String email,
-                         @RequestParam(required = false) String phone,
+                         RedirectAttributes redir,
                          Model model) {
         if (userService.existsByLogin(login)) {
-            model.addAttribute("exists", true);
-            return "register";
+            redir.addFlashAttribute("exists", true);
+            return "redirect:/login";
         }
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String passHash = passwordEncoder.encode(password);
         List<Basket> baskets = new ArrayList<>();
-        CustomUser dbUser = new CustomUser(login, passHash, UserRole.USER, email, phone, baskets);
+        CustomUser dbUser = new CustomUser(login, passHash, UserRole.USER, email, baskets);
         userService.addUser(dbUser);
         String basketName = login + "Basket" + dbUser.getBasketNumber();
         Basket basket = new Basket(basketName, dbUser);
         basketService.saveBasket(basket);
         return "redirect:/";
     }
+
+    @GetMapping("/{id}")
+    public String findMobilesByBrand(@PathVariable(value = "id") long brandId,
+                                     @RequestParam(required = false, defaultValue = "0") Integer page,
+                                     Model model) {
+        Brand brand = brandService.findBrandById(brandId);
+        if (page < 0) page = 0;
+        List<Mobile> mobiles = mobileService.findByBrand(brand, new PageRequest(page, ITEMS_PER_PAGE, Sort.Direction.DESC, "id"));
+        model.addAttribute("brands", brandService.findBrands());
+        model.addAttribute("mobiles", mobiles);
+        model.addAttribute("byGroupPages", getPageCount(brand));
+        model.addAttribute("brandId", brandId);
+        return "index";
+    }
+
+    private long getPageCount(Brand brand) {
+        long totalCount = mobileService.countByBrand(brand);
+        return (totalCount / ITEMS_PER_PAGE) + ((totalCount % ITEMS_PER_PAGE > 0) ? 1 : 0);
+    }
+
 }
