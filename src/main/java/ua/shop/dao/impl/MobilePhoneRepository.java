5ce9beabee5c0d0c21a81b17a1a3e3979179d7b6
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-03-03 21:00:13 +0200

    Change MobilePhoneRepository

--- src/main/java/ua/shop/dao/impl/MobilePhoneRepository.java
+++ src/main/java/ua/shop/dao/impl/MobilePhoneRepository.java
@@ -3,6 +3,7 @@ package ua.shop.dao.impl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.shop.dao.Mobile;
 import ua.shop.dao.MobilePhone;
+import ua.shop.dao.PhoneStatus;
 
 import java.util.List;
 
@@ -10,7 +11,12 @@ public interface MobilePhoneRepository extends JpaRepository<MobilePhone, Long> 
 
     MobilePhone findById(long id);
 
-    MobilePhone getFirstByBasketIsNullAndMobile(Mobile mobile);
+
+//    MobilePhone getFirstByBasketIsNullAndMobile(Mobile mobile);
+    MobilePhone getFirstByMobileAndStatus(Mobile mobile, PhoneStatus status);
+
+//    MobilePhone getFirstByMobileAndStatus(Mobile mobile, String status);
+//    MobilePhone getMobilePhoneByMobileAndStatus(Mobile mobile, String status);
 
     List<MobilePhone> findByBasketIsNullAndMobile(Mobile mobile);
 
