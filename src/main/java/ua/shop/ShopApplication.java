f85db544311c7d1b93f99c2481f9e1a02bdcada8
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-03-12 10:35:41 +0200

    Change admin password to "admin"

--- src/main/java/ua/shop/ShopApplication.java
+++ src/main/java/ua/shop/ShopApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import ua.shop.dao.Basket;
 import ua.shop.dao.CustomUser;
-import ua.shop.dao.Mobile;
 import ua.shop.dao.UserRole;
 import ua.shop.service.MobileService;
 import ua.shop.service.UserService;
@@ -26,7 +25,7 @@ public class ShopApplication {
             @Override
             public void run(String... strings) throws Exception {
                 List<Basket> list = new ArrayList<>();
-                userService.addUser(new CustomUser("admin", "$2a$08$bgpF09DGbsGslB3IDOrTTeETbi2gOzPfi/JcwpAo6oxFZbKXNAI3K", UserRole.ADMIN, "admin@gmailcom", list));
+                userService.addUser(new CustomUser("admin", "$2a$08$.JUplF24HwIZW7XPgo.94uRQHYse2HGYNq4z.5nQllco7O.EyHSu.", UserRole.ADMIN, "admin@gmailcom", list));
             }
         };
     }
