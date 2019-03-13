114280d3f243c4bd7edc991dfa10d01368f9cacb
Vasyl Kobylchak <v.kobylchak@ukr.net>
2019-03-13 18:31:17 +0200

    notification service

--- src/main/java/ua/shop/controller/RegistrationController.java
+++ src/main/java/ua/shop/controller/RegistrationController.java
@@ -1,29 +1,29 @@
-package ua.shop.controller;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ua.shop.dao.CustomUser;
-import ua.shop.service.NotificationService;
-
-@RestController
-public class RegistrationController {
-    private Logger logger = LoggerFactory.getLogger(RegistrationController.class);
-    @Autowired
-    private NotificationService notificationService;
-
-    @RequestMapping("/report")
-    public String signupSuccess() {
-        CustomUser shop = new CustomUser();
-        shop.setEmail("beonconnection@gmail.com");
-        try {
-            notificationService.sendNotification(shop);
-        } catch (MailException e) {
-            logger.info("Error Sending Email: " + e.getMessage());
-        }
-        return "Thank you for registering with us.";
-    }
-}
+//package ua.shop.controller;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.MailException;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import ua.shop.dao.CustomUser;
+//import ua.shop.service.NotificationService;
+//
+//@RestController
+//public class RegistrationController {
+//    private Logger logger = LoggerFactory.getLogger(RegistrationController.class);
+//    @Autowired
+//    private NotificationService notificationService;
+//
+//    @RequestMapping("/report")
+//    public String signupSuccess() {
+//        CustomUser shop = new CustomUser();
+//        shop.setEmail("beonconnection@gmail.com");
+//        try {
+//            notificationService.sendNotification(shop);
+//        } catch (MailException e) {
+//            logger.info("Error Sending Email: " + e.getMessage());
+//        }
+//        return "Thank you for registering with us.";
+//    }
+//}
--- src/main/java/ua/shop/service/NotificationService.java
+++ src/main/java/ua/shop/service/NotificationService.java
@@ -1,31 +1,31 @@
-package ua.shop.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-import ua.shop.dao.CustomUser;
-
-@Service
-public class NotificationService {
-
-    private JavaMailSender javaMailSender;
-
-    @Autowired
-    public NotificationService(JavaMailSender javaMailSender){
-        this.javaMailSender = javaMailSender;
-    }
-
-    public void sendNotification(CustomUser user)throws MailException {
-        SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setTo("vasylkobylchak@gmail.com");
-        mail.setFrom("beonconnection@gmail.com");
-        StringBuilder result = new StringBuilder();
-        result.append("na obrabotky").append(System.lineSeparator()).append("user name + ").append(System.lineSeparator()).append("bye");
-        mail.setSubject("na obrabotky");
-        mail.setText(result.toString());
-        javaMailSender.send(mail);
-    }
-
-}
+//package ua.shop.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.MailException;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//import ua.shop.dao.CustomUser;
+//
+//@Service
+//public class NotificationService {
+//
+//    private JavaMailSender javaMailSender;
+//
+//    @Autowired
+//    public NotificationService(JavaMailSender javaMailSender){
+//        this.javaMailSender = javaMailSender;
+//    }
+//
+//    public void sendNotification(CustomUser user)throws MailException {
+//        SimpleMailMessage mail = new SimpleMailMessage();
+//        mail.setTo("vasylkobylchak@gmail.com");
+//        mail.setFrom("beonconnection@gmail.com");
+//        StringBuilder result = new StringBuilder();
+//        result.append("na obrabotky").append(System.lineSeparator()).append("user name + ").append(System.lineSeparator()).append("bye");
+//        mail.setSubject("na obrabotky");
+//        mail.setText(result.toString());
+//        javaMailSender.send(mail);
+//    }
+//
+//}
